17fe0e07c309ad7d4a01589c19a59e6ce80d8080
ElliotKoch <kochelliotpro@gmail.com>
2025-02-18 14:36:30 +0100

    Time: 0 ms (100%), Space: 17.8 MB (38.5%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,11 +1,14 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
+        # Initialize a check list to check redundant numbers
         check = []
         while n != 1:
             digits = str(n)
             n = 0
+            # Sum of the digits of n to the power of 2
             for digit in digits:
                 n += int(digit)**2
+            # If n was encounter, return False
             if n in check:
                 return False
             else:
