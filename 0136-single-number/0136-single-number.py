1e56d2b38686cb4df565eddbea1cdb843326e720
ElliotKoch <kochelliotpro@gmail.com>
2025-02-18 14:11:10 +0100

    Time: 10 ms (20.44%), Space: 19.4 MB (99.06%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,12 +1,16 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        stack = []
-        for num in nums:
-            if num in stack:
-                stack.remove(num)
-            else:
-                stack.append(num)
-        return stack[0]
+        # Sorts the list
+        nums.sort()
+        # Iterate through the num two by two
+        for i in range(0, len(nums), 2):
+            # In case the unique number is the last one
+            if i == len(nums)-1:
+                return nums[i]
+            # Check that the next number is different
+            elif nums[i] != nums[i+1]:
+                return nums[i]
+        
 
 
         
