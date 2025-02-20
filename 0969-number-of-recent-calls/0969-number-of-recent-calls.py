b154e6718cb8161cbf5ed116c8481558da77707e
ElliotKoch <kochelliotpro@gmail.com>
2025-02-20 11:31:11 +0100

    Time: 3015 ms (8.81%), Space: 23.1 MB (76.8%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.py
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.py
@@ -2,7 +2,7 @@ class RecentCounter:
 
     def __init__(self):
         self.requests = []
-        self.index = -1
+        self.index = 0
         
 
     def ping(self, t: int) -> int:
@@ -12,11 +12,11 @@ class RecentCounter:
         
         # Count the number of requests that are in the interval [t - 3000, t]
         self.requests.append(t)
-        for i in range(self.index+1,len(self.requests)):
+        for i in range(len(self.requests)):
             if self.requests[i] >= t-3000 and self.requests[i] <= t:
                 past_requests += 1
             else:
                 # Remember the index of the "old" requests to bybass them at the next call of ping
                 self.index = i
-
+        self.requests = self.requests[self.index:]
         return past_requests
