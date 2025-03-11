81396414528614bb1931d8ca158c5c68d026d176
ElliotKoch <kochelliotpro@gmail.com>
2025-03-11 16:31:52 +0100

    Time: 8 ms (53.53%), Space: 17.9 MB (83.67%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # Define a variable for s without punctuations
+        s_no_punct = ""
+        # Filter s with isalpha to retrieve only alphabetical and number characters
+        for c in s:
+            if c.isalpha():
+                s_no_punct += c.lower()
+            elif ord(c) > 47 and ord(c) < 58:
+                s_no_punct += c
+        # Check that it is a palindrome
+        if s_no_punct == s_no_punct[::-1]:
+            return True
+        else:
+            return False
