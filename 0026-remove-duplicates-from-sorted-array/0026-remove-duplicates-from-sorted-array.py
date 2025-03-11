011d13a1cf5ba6167018bfa03e06ecdc35e00aa0
ElliotKoch <kochelliotpro@gmail.com>
2025-03-11 16:09:03 +0100

    Time: 0 ms (100%), Space: 19 MB (30.69%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,21 +1,12 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        # Initialize a counter for the removed number and the index pointer
-        counter_del_ele = 0
-        i = 0
-        # Escaping condition is to check if we are at the end of the list
-        while nums[i] != '_' and len(nums)>1 and i != len(nums)-1:
-            # Ensure that the number after is not the same
-            if nums[i] == nums[i+1]:
-                # If it is the case we delete it and we add underscore at the end of the list
-                nums.pop(i)
-                nums.append('_')
-                # Increament counter for deleted item
-                counter_del_ele += 1
-            else:
-                # If no duplicate, we increased counter
-                i += 1
-            
-        # return the len of nums minus de number of removed number
-        return len(nums)-counter_del_ele
-        
+        # Initialize a pointer for the position of the next unique number to be placed in the list nums
+        unique_nb = 1
+        # Iterate through the list nums
+        for i in range(1, len(nums)):
+            # In case we have a unique number i
+            if nums[i] != nums[i - 1]:
+                # Place it at the unique_nb position and increment the pointer by 1 
+                nums[unique_nb] = nums[i]
+                unique_nb += 1
+        return unique_nb # Correspond to the first unique numbers in the list nums
